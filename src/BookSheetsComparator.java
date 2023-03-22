ccd2dd3f303af44d82a9f1726b9fd42ab49059d3
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-23 00:14:50 +0100

    add BookSheetsComparator

---
+++ src/BookSheetsComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class BookSheetsComparator implements Comparator<Book> {
+  @Override
+  public int compare(Book o1, Book o2) {
+    if (o1.getSheets() - o2.getSheets() != 0) {
+            return o1.getSheets()-o2.getSheets();
+    }
+    return o1.getTitle().compareTo(o1.getTitle());
+  }
+}
