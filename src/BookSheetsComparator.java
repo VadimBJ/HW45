8ea8a61b149b2d1e403b4e33fa55b0624bef8c28
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-23 00:21:09 +0100

    fix BookSheetsComparator

--- src/BookSheetsComparator.java
+++ src/BookSheetsComparator.java
@@ -4,7 +4,7 @@ public class BookSheetsComparator implements Comparator<Book> {
   @Override
   public int compare(Book o1, Book o2) {
     if (o1.getSheets() - o2.getSheets() != 0) {
-            return o1.getSheets()-o2.getSheets();
+            return o2.getSheets()-o1.getSheets();
     }
     return o1.getTitle().compareTo(o1.getTitle());
   }
