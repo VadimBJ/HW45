a3281f8cdca28a2410818dba853954a2df3c3c84
Vadim Balanciuc <b.vadik2017@gmail.com>
2023-03-22 23:59:44 +0100

    add BookTitleComparator

---
+++ src/BookTitleComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class BookTitleComparator implements Comparator<Book> {
+
+  @Override
+  public int compare(Book o1, Book o2) {
+    if (!o1.getTitle().equals(o2.getTitle())){
+      return o1.getTitle().compareTo(o2.getTitle());
+    }
+    return o1.getAuthor().compareTo(o2.getAuthor());
+  }
+}
